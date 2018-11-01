abaed18d2b6ca671e8460309fa3b58deff480221
Zer0-M <mjamil90@stuy.edu>
2018-11-01 10:58:51 -0400

    testing recursive checking of subdirectories

--- main.c
+++ main.c
@@ -5,11 +5,9 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
-// main function
-int main() {
-    DIR* d;
-    d = opendir("./");
+void our_ls(DIR * d){
     struct dirent* entry;
     entry = readdir(d);
     int size = 0;
@@ -17,6 +15,12 @@ int main() {
         struct stat* b = malloc(sizeof(struct stat));
         if (entry->d_type == DT_DIR)
             printf("directory: %s\n", entry->d_name);
+            if(!(strcmp(entry->d_name,"./"))||!(strcmp(entry->d_name,"../"))){
+                DIR * sub;
+                sub=opendir(entry->d_name);
+                our_ls(sub);
+                closedir(sub);
+            }
         if (entry->d_type == DT_REG) {
             printf("regular: %s\n", entry->d_name);
             stat(entry->d_name, b);
@@ -26,6 +30,12 @@ int main() {
         free(b);
     }
     printf("total size of not directory files: %d\n", size);
+}
+// main function
+int main() {
+    DIR* d;
+    d = opendir("./");
+    our_ls(d);
     closedir(d);
 }
 
