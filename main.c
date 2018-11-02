6f24b90fc88bf188b690d9c2183bf4b660b5c368
Hanna Yang <hyang6@stuy.edu>
2018-11-02 10:59:03 -0400

    hmm

--- main.c
+++ main.c
@@ -6,45 +6,57 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <errno.h>
 
 int our_ls(DIR * d){
     if (d) {
         struct dirent* entry;
         entry = readdir(d);
-        int size = 0;
-        printf("STATISTICS FOR DIRECTORY: \n");
-        int i = 0;
-        while (entry) {
-            i ++;
-            struct stat* b = malloc(sizeof(struct stat));
-            if (entry->d_type == DT_DIR) {
-                printf("directory: %s\n", entry->d_name);
-                /*if (i > 2) {*/
-                    /*DIR * sub;*/
-                    /*sub = opendir(entry->d_name);*/
-                    /*size += our_ls(sub);*/
-                    /*closedir(sub);*/
-                /*}*/
+        if (entry) {
+            int size = 0;
+            printf("STATISTICS FOR DIRECTORY: \n");
+            int i = 0;
+            while (entry) {
+                i ++;
+                struct stat* b = malloc(sizeof(struct stat));
+                if (entry->d_type == DT_DIR) {
+                    printf("directory: %s\n", entry->d_name);
+                    /*if (i > 2) {*/
+                        /*DIR * sub;*/
+                        /*sub = opendir(entry->d_name);*/
+                        /*size += our_ls(sub);*/
+                        /*closedir(sub);*/
+                    /*}*/
+                }
+                if (entry->d_type == DT_REG) {
+                    printf("regular: %s\n", entry->d_name);
+                    stat(entry->d_name, b);
+                    size += b->st_size;
+                }
+                entry = readdir(d);
+                free(b);
             }
-            if (entry->d_type == DT_REG) {
-                printf("regular: %s\n", entry->d_name);
-                stat(entry->d_name, b);
-                size += b->st_size;
-            }
-            entry = readdir(d);
-            free(b);
+            printf("total size of not directory files: %d\n", size);
+            return size;
         }
-        printf("total size of not directory files: %d\n", size);
-        return size;
+    } else {
+        printf("error: %s", strerror(errno));
     }
+
     return 0; 
 }
 // main function
-int main() {
-    DIR* d;
-    d = opendir("./");
-    our_ls(d);
-    closedir(d);
+int main(int argc, char* argv[]) {
+    if (argc >1 ) {
+        DIR* d;
+        d = opendir(argv[1]);
+        our_ls(d);
+        closedir(d);
+    } else {
+        printf("please specify directory:\n");
+        
+    }
+    return 0;
 }
 
 
